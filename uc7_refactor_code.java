0199a8324b58dfae00a5ed18ecf8a625b2b638d2
mahesh-tippanu <102399228+mahesh-tippanu@users.noreply.github.com>
2022-04-27 12:18:57 +0500

    Update uc7_refactor_code.java
    added formulas

--- uc7_refactor_code.java
+++ uc7_refactor_code.java
@@ -1,13 +1,15 @@
-package com.example.employeeWage;
-
+package com.example.uc7_refactor_code;
 import java.util.ArrayList;
 import java.util.Scanner;
 
-interface EmpWageBuilder{
+interface EmpWageBuilder
+{
     void empWageBuilder();
 }
 
-public class EmployeeWage implements EmpWageBuilder {
+public class uc7_refactor_code implements EmpWageBuilder
+{
+    //interger function is getting instialized
     final int FULL_TIME = 1;
     final int PART_TIME = 2;
     int wagePerHour;
@@ -31,6 +33,7 @@ public class EmployeeWage implements EmpWageBuilder {
         this.maxHoursInMonth = maxHoursInMonth;
     }
     public void setValues() {
+       //scanner function is introduced
         Scanner sc = new Scanner(System.in);
         System.out.println("Enter Company Name : ");
         setCompanyName(sc.nextLine());
@@ -41,13 +44,16 @@ public class EmployeeWage implements EmpWageBuilder {
         System.out.println("Enter maximum hours in a month : ");
         setMaxHoursInMonth(sc.nextInt());
     }
-    public void computeEmployeeWage() {
-        int empHrs = 0;
-        int empWage = 0;
+    public void computeEmployeeWage()
+    {
+        //integer function is called
+        int empHrs = 1;
+        int empWage = 2;
         int totalEmpWage = 0;
         int employeeWorkingHrs = 0;
-
-        for (int day = 0; day < workingDaysInMonth && employeeWorkingHrs < maxHoursInMonth; day++) {
+       //for_loop is getting intialized
+        for (int day = 0; day < workingDaysInMonth && employeeWorkingHrs < maxHoursInMonth; day++)
+        {
             int empCheck = (int) Math.floor(Math.random() * 10) % 3;
             if (empCheck == FULL_TIME)
                 empHrs = 8;
@@ -55,7 +61,10 @@ public class EmployeeWage implements EmpWageBuilder {
                 empHrs = 4;
             else
                 empHrs = 0;
-
+            //formula :
+            // 1) a2 = a1 * b1;
+            // b1 += a2;
+            // b2 += a1;
             empWage = empHrs * wagePerHour;
             totalEmpWage += empWage;
             employeeWorkingHrs += empHrs;
@@ -64,10 +73,11 @@ public class EmployeeWage implements EmpWageBuilder {
         }
         System.out.println("Total Wage of Employee in " + companyName + " is : " + totalEmpWage);
     }
-    public void empWageBuilder() {
+    public void empWageBuilder()
+    {
         ArrayList<Object> empWageArray = new ArrayList<>();
-        EmployeeWage company1 = new EmployeeWage();
-        EmployeeWage company2 = new EmployeeWage();
+        uc7_refactor_code company1 = new uc7_refactor_code();
+        uc7_refactor_code company2 = new uc7_refactor_code();
         company1.setValues();
         String nameOfCompany1 = company1.companyName;
         company2.setValues();
@@ -75,6 +85,7 @@ public class EmployeeWage implements EmpWageBuilder {
         Scanner sc = new Scanner(System.in);
         System.out.println("Enter company name to fetch total wage : ");
         String nameCompany = sc.nextLine();
+        //if statement is used
         if(nameOfCompany1.equalsIgnoreCase(nameCompany)){
             company1.computeEmployeeWage();
         }
@@ -85,7 +96,7 @@ public class EmployeeWage implements EmpWageBuilder {
         empWageArray.add(company2);
     }
     public static void main(String[] args) {
-        EmpWageBuilder empWageObj = new EmployeeWage();
+        EmpWageBuilder empWageObj = new uc7_refactor_code();
         empWageObj.empWageBuilder();
     }
 }
